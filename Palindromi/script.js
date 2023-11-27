34c63219921a6081ff9aee84ce0f2171bb9d6cb2
Matteo Fantacuzzi <fantacuzzimatteo@gmail.com>
2023-11-27 14:52:29 +0100

    end

--- Palindromi/script.js
+++ Palindromi/script.js
@@ -3,7 +3,36 @@ Chiedere all’utente di inserire una parola
 Creare una funzione per capire se la parola inserita è palindroma
 */
 
+// chiedere all'utente una parola
+const parola = prompt("Inserisci una parola");
+console.log(parola);
 
-const askWord = prompt("Inserisci una parola");
-console.log(askWord);
+// caratteri della parola
+let carattere
 
+
+// parola inversa
+let parolaInvertita = "";
+
+
+// elemento
+let i = parola.length - 1;
+
+
+// ciclo while
+while (i >= 0) {
+    carattere = parola[i];
+    parolaInvertita += carattere;
+    console.log(carattere);
+    i--;
+}
+
+console.log(parolaInvertita);
+
+
+// esito
+if(parola == parolaInvertita){
+    console.log('la parola è palindroma');
+} else {
+    console.log('la parola non è palindroma');
+}
