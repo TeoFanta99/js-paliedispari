23a85256587059f156b70654d156c9a3b94e1498
Matteo Fantacuzzi <fantacuzzimatteo@gmail.com>
2023-11-27 14:31:07 +0100

    add const askWord js

--- Palindromi/script.js
+++ Palindromi/script.js
@@ -1,2 +1,9 @@
+/*
+Chiedere all’utente di inserire una parola
+Creare una funzione per capire se la parola inserita è palindroma
+*/
 
+
+const askWord = prompt("Inserisci una parola");
+console.log(askWord);
 
